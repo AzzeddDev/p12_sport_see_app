792a8d4f2c0fb56fa8d79d3a7f56e17612c637aa
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-03-12 11:08:30 +0100

    update navbar - header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,17 @@
 const Header = () => {
     return (
         <>
-            <div className={"container"}>header</div>
+            <header className={"container-fluid navBar"}>
+                <navbar>
+                    <div className={"container d-flex justify-content-between align-items-center"}>
+                        <img className={"navBar__logo"} src="../../../src/assets/img/logo.png" alt=""/>
+                        <a className={"navBar__link"} href="#">Accueil</a>
+                        <a className={"navBar__link"} href="#">Profil</a>
+                        <a className={"navBar__link"} href="#">Réglage</a>
+                        <a className={"navBar__link"} href="#">communauté</a>
+                    </div>
+                </navbar>
+            </header>
         </>
     )
 }
