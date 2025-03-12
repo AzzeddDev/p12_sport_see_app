719b7fcf851afe02503e3710fbec13e8a4cb8420
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-03-12 11:10:11 +0100

    add a home template with links to users dashboard

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <div className={"container p-5"}>
+            <div className={"d-flex gap-3"}>
+                <a href="/dashboard/12">Link 12</a>
+                <a href="/dashboard/18">Link 18</a>
+            </div>
+        </div>
+    )
+}
+
+export default Home
