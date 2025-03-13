368393898491564635703565101d50a45e91a8aa
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-03-13 08:51:46 +0100

    add states for get perfomance & average session

--- src/hooks/useUserHook/index.ts
+++ src/hooks/useUserHook/index.ts
@@ -1,21 +1,35 @@
 import { useEffect, useState } from "react"
-import {getUserInfo} from "../../api/api.ts"
+import {getUserAverageSessions, getUserInfo, getUserPerformance} from "../../api/api.ts"
 
 export const useUserData = (userId: number) => {
     const [userData, setUserData] = useState<any>(null)
+    const [performance, setPerformance] = useState<any>(null)
+    const [averageSession, setAverageSessions] = useState<any>(null)
+
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const data = await getUserInfo(userId)
-                setUserData(data)
-            } catch (error) {
-                console.error("Failed to fetch user data", error)
+                setLoading(true)
+                const userInfo = await getUserInfo(userId)
+                const userPerformance = await getUserPerformance(userId)
+                const userAverageSessions = await getUserAverageSessions(userId)
+
+                setUserData(userInfo)
+                setPerformance(userPerformance)
+                setAverageSessions(userAverageSessions)
+            } catch (err) {
+                console.error("Failed to fetch user data", err)
+                setError("Failed to load user data")
+            } finally {
+                setLoading(false)
             }
         }
 
         fetchData()
     }, [userId])
 
-    return { userData }
+    return { userData, performance, averageSession, loading, error }
 }
